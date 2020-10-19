62add29f85cb9aab147fca4e063415194f92547a
Anna Agozzino <anna.agozzino91@gmail.com>
2020-10-19 17:46:19 +0200

    Test css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -148,6 +148,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Pasta Molisana</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
     <body>
         <h2>Lunga</h2>
