b4d109f9ce8178bd1facce0e78d3224355b5322f
Anna Agozzino <anna.agozzino91@gmail.com>
2020-10-20 16:56:43 +0200

    Grafica Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,52 @@
+{{-- FOOTER --}}
+<footer>
+    <div class="top">
+        <div class="wrapper">
+            <div class="row">
+                <div class="box">
+                    <div class="logo">
+                        <img src="{{asset('images/marchio.png')}}" alt="Logo La Molisana">
+                    </div>
+                    <div class="text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem blanditiis repudiandae facilis beatae doloribus facere eos commodi recusandae, incidunt, culpa cumque sunt voluptatibus reprehenderit sit quo, rerum perspiciatis pariatur adipisci!</div>
+                </div>
+                <div class="box">
+                    <h3 class="box-title">Pastificio</h3>
+                    <ul class="box-list">
+                        <li class="box-list-item"><a href="#">Il Pastificio</a></li>
+                        <li class="box-list-item"><a href="#">Grano decorticato a pietra</a></li>
+                        <li class="box-list-item"><a href="#">Il Molise c'è</a></li>
+                        <li class="box-list-item"><a href="#">Filiera Integrata</a></li>
+                        <li class="box-list-item"><a href="#">100 anni di pasta</a></li>
+                        <li class="box-list-item"><a href="#">Sartoria della pasta</a></li>
+                        <li class="box-list-item"><a href="">Spaghetto Quadrato</a></li>
+                        <li class="box-list-item"><a href="">Le Persone</a></li>
+                        <li class="box-list-item"><a href=""></a></li>
+                    </ul>
+                    <h3 class="box-title">Prodotti</h3>
+                    <ul class="box-list">
+                        <li class="box-list-item"><a href="#">Le Classiche</a></li>
+                        <li class="box-list-item"><a href="#">Le Integrali</a></li>
+                        <li class="box-list-item"><a href="#">Le Speciali</a></li>
+                        <li class="box-list-item"><a href="#">Le Biologiche</a></li>
+                        <li class="box-list-item"><a href="#">Le Gluten-Free</a></li>
+                        <li class="box-list-item"><a href="#">Le Semole</a></li>
+                        <li class="box-list-item"><a href="#">Le Extra di Lusso</a></li>
+                    </ul>
+                </div>
+                <div class="box">
+                    <h3 class="box-title">Collezioni da chef</h3>
+                    <ul class="box-list">
+                        <li class="box-list-item"><a href="#">Collezione da Chef</a></li>
+                        <li class="box-list-item"><a href="#">Grandi Cucine</a></li>
+                        <li class="box-list-item"><a href="#">Biologiche</a></li>
+                        <li class="box-list-item"><a href="#">Quadrate</a></li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div> 
+    <div class="bottom">
+    </div>   
+</footer>
+{{-- /FOOTER --}}
