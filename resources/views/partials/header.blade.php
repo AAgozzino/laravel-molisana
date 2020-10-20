0cf13de8f8e21cc8b2dddeb62a891d9c51530106
Anna Agozzino <anna.agozzino91@gmail.com>
2020-10-20 16:04:19 +0200

    Grafica Header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,18 +2,18 @@
 <header>
     <div class="wrapper">
         <div class="logo">
-            <img src="" alt="">
+            <img src="{{asset('images/marchio.png')}}" alt="Logo La Molisana">
         </div>
-        <nav class="main-nav">
-            <ul class="main-nav-list">
-                <li class="main-nav-list-item">
-                    <a href="#">Home</a>
+        <nav class="nav">
+            <ul class="nav-list">
+                <li class="nav-list-item">
+                    <a href="{{route('home')}}">Home</a>
                 </li>
-                <li class="main-nav-list-item">
-                    <a href="#">Prodotti</a>
+                <li class="nav-list-item">
+                    <a class="active" href="{{route('products')}}">Prodotti</a>
                 </li>
-                <li class="main-nav-list-item">
-                    <a href="#">News</a>
+                <li class="nav-list-item">
+                    <a href="{{route('news')}}">News</a>
                 </li>
             </ul>
         </nav>
